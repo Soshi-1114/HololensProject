4f5d467e05c315557a1532cb025e0a517f42d9d9
Soshi-1114 <4649chopper@gmail.com>
2021-11-02 12:25:49 +0900

    add agent type drone

--- Assets/Scripts/Navigate.cs
+++ Assets/Scripts/Navigate.cs
@@ -8,15 +8,14 @@ public class Navigate : MonoBehaviour
     public NavMeshAgent navigater;
     public GameObject evacuee;
     public GameObject target;
-    private Animator animator;
     [SerializeField] bool isTargetTracking = false;
-    [SerializeField] bool isEvacueeTracking = true;
-    [SerializeField] float trackedRange = 0.5f;
-    [SerializeField] float  navigatingRange = 2.0f;
+    // [SerializeField] bool isEvacueeTracking = true;
+    [SerializeField] float targetRange = 0.5f;
+    [SerializeField] float  evacueeRange = 0.5f;
+    [SerializeField] float  agentFeet = 6.0f;
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -25,66 +24,22 @@ public class Navigate : MonoBehaviour
         var navigaterPoint = new Vector3(navigater.transform.position.x, transform.position.y, navigater.transform.position.z);
         var evacueePoint = new Vector3(evacuee.transform.position.x, transform.position.y, evacuee.transform.position.z);
         var targetPoint = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
-        // var navigaterPoint = navigater.transform.position;
-        // var evacueePoint = evacuee.transform.position;
-        // var targetPoint = target.transform.position;
 
-        var NtoTDis = Vector3.Distance( targetPoint, navigaterPoint );
-        var NtoEDis = Vector3.Distance( evacueePoint, navigaterPoint );
-
-        if ( NtoTDis >= trackedRange ){
-            navigater.destination = targetPoint;
-            if ( NtoEDis > navigatingRange ){
+        var targetDistance = Vector3.Distance( targetPoint, navigaterPoint );
+        var evacueeDistance = Vector3.Distance( evacueePoint, navigaterPoint );
+        
+        if ( isTargetTracking ){
+            if ( evacueeDistance >= evacueeRange ){
+                isTargetTracking = false;
                 navigater.destination = evacueePoint;
             }
+            navigater.destination = targetPoint;
         }
         else {
-            if ( NtoEDis > navigatingRange ){
-                navigater.destination = evacueePoint;
+            if ( targetDistance >= targetRange ){
+                isTargetTracking = true;
             }
         }
         
-        // if ( NtoTDis >= trackedRange) {
-        //     if ( NtoEDis <= trackedRange ) {
-        //         animator.SetBool("walking", true);
-        //         navigater.destination = targetPoint;
-        //     } 
-        //     else if ( NtoEDis > navigatingRange ){
-        //         animator.SetBool("walking", true);
-        //         navigater.destination = evacueePoint;
-        //     } 
-        //     else {
-        //         animator.SetBool("walking", false);
-        //     }
-        // } 
-        // else {
-        //     if ( NtoEDis > navigatingRange ){
-        //         navigater.destination = evacueePoint;
-        //         animator.SetBool("walking", true);
-        //     } 
-        //     animator.SetBool("walking", false);
-        // }
-
-    //     if ( isTargetTracking ){
-    //         if ( NtoEDis > trackedRange ){
-    //             isTargetTracking = false;
-    //             isEvacueeTracking = true;
-    //             if ( isEvacueeTracking ){
-    //                 if ( NtoEDis <= trackedRange ){
-    //                     isTargetTracking = true;
-    //                     isEvacueeTracking = false;
-    //                 }
-    //                 navigater.destination = evacueePoint;
-    //                 // animator.SetBool("walking", true);
-    //             }
-    //         }
-    //         navigater.destination = targetPoint;
-    //         // animator.SetBool("walking", true);
-    //     }
-    //     else {
-    //         if ( NtoEDis <= trackedRange ){
-    //             isTargetTracking = true;
-    //         }
-    //     }
     }
 }
