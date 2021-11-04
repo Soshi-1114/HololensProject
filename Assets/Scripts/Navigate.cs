589004bbf2f0710d13c1029e2d2aa8d70a6e1478
Soshi-1114 <4649chopper@gmail.com>
2021-11-04 17:07:30 +0900

    create new scene

--- Assets/Scripts/Navigate.cs
+++ Assets/Scripts/Navigate.cs
@@ -10,14 +10,13 @@ public class Navigate : MonoBehaviour
     public GameObject evacuee;
     public GameObject target;
     public TextMeshProUGUI _debug;
-    // [SerializeField] bool isTargetTracking = false;
-    // [SerializeField] bool isEvacueeTracking = true;
-    // [SerializeField] float targetRange = 0.5f;
-    // [SerializeField] float  evacueeRange = 0.1f;
-    // [SerializeField] float  agentFeet = 6.0f;
-    // Start is called before the first frame update
+    int n = 0;
     void Start()
     {
+        navigater = transform.GetComponent<NavMeshAgent>();
+        var targetPoint = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+        var evacueePoint = new Vector3(evacuee.transform.position.x, transform.position.y, evacuee.transform.position.z);
+        navigater.destination = evacueePoint; 
     }
 
     private void Update()
@@ -25,39 +24,41 @@ public class Navigate : MonoBehaviour
         navigater = transform.GetComponent<NavMeshAgent>();
         var navigaterPoint = new Vector3(navigater.transform.position.x, transform.position.y, navigater.transform.position.z);
         var evacueePoint = new Vector3(evacuee.transform.position.x, transform.position.y, evacuee.transform.position.z);
-        var targetPoint = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);;
+        var targetPoint = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
         var targetDistance = Vector3.Distance( targetPoint, navigaterPoint );
         var evacueeDistance = Vector3.Distance( evacueePoint, navigaterPoint );
         
-        if(targetDistance >= evacueeDistance){
-            navigater.destination = targetPoint;
-            // _debug.text = "target\n";
-        } 
-        // else if(targetDistance <= 0.2f){
-        //     navigater.destination = ;
-        // }
-        else{
-            navigater.destination = evacueePoint;
-            if(evacueeDistance < 0.1f){
+        if(evacueeDistance <= 0.5f){
+            if(targetDistance >= 1f){
                 navigater.destination = targetPoint;
+                navigater.speed = 1f;
+                navigater.updateRotation = true;
+            }else{
+                //evacueeに近付くと止まる
+                navigater.speed = 0f;
+                navigater.updateRotation = false;   
             }
-            // _debug.text = "evacuee\n";
+            
+        }else{
+            navigater.destination = evacueePoint;
+            navigater.speed = 1f;
+            navigater.updateRotation = true;
         }
-        // if ( isTargetTracking ){
-        //     if ( evacueeDistance >= evacueeRange ){
-        //         isTargetTracking = false;
-        //         _debug.text = "\nisTargetTracking:evacueeDistance\n"+isTargetTracking.ToString()+evacueeDistance.ToString();
-        //         navigater.destination = evacueePoint;
-        //     }
+
+
+        // if(targetDistance >= evacueeDistance){
         //     navigater.destination = targetPoint;
-        // }
-        // else {
-        //     if ( targetDistance >= targetRange ){
-        //         isTargetTracking = true;
-        //         _debug.text = "\nisTargetTracking:targetDistance\n"+isTargetTracking.ToString()+targetDistance.ToString();
-        //     }
+        //     // _debug.text = "target\n";
+        // } 
+        // // else if(targetDistance <= 0.2f){
+        // //     navigater.destination = ;
+        // // }
+        // else{
         //     navigater.destination = evacueePoint;
+        //     if(evacueeDistance < 0.05f){
+        //         navigater.destination = targetPoint;
+        //     }
+        //     // _debug.text = "evacuee\n";
         // }
-        
     }
 }
